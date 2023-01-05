037af63f1b4ddb240c7f57f84f3494f2be93703c
Cesar Ruiz <ces.ogando@hotmail.com>
2023-01-05 16:53:18 -0600

    Vinculada Tareas a app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import colors from 'colors';
 import { inquirerMenu, pausa } from './helpers/inquirer.js';
 import { Tarea } from './models/tarea.js';
 import { v4 as uuidv4 } from 'uuid';
+import { Tareas } from './models/tareas.js';
 uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
  
 console.clear();
@@ -9,11 +10,10 @@ console.clear();
 const main = async () => {
   let opt = '';
   do {
-   //  opt = await inquirerMenu();
-   //  console.log({ opt });
-    const tarea = new Tarea ('Comprar comida');
-    console.log(tarea);
-    
+   opt = await inquirerMenu();
+   console.log({ opt });
+    tareas._listado[tarea.id] = tarea;
+    console.log(tareas);
 
 
     await pausa();
